49059acd669993d4020c3095a532caf05acf6f5f
3083570060@qq.com <3083570060@qq.com>
2023-03-28 16:21:14 +0800

    实现echarts数据X轴滚动以及柱状图上加line

--- src/config/EchartBar.vue
+++ src/config/EchartBar.vue
@@ -20,6 +20,10 @@ const props = defineProps({
     type: Array,
     required: false,
   },
+  dataLine: {
+    type: Array,
+    required: false
+  }
 });
 onMounted(() => {
   // 基于准备好的dom，初始化echarts实例
@@ -30,6 +34,33 @@ onMounted(() => {
       text: props.name,
     },
     tooltip: {},
+    // x轴数据滚动
+    dataZoom: [
+      {
+        type: 'slider',
+        show: false,
+        start: 54,
+        end: 100,
+        handleSize: 1
+      },
+      {
+        type: 'inside',
+        start: 94,
+        end: 100,
+      },
+      // y轴滚动
+      // {
+      //   type: 'slider',
+      //   show: true,
+      //   yAxisIndex: 0,
+      //   filterMode: 'empty',
+      //   width: 0,
+      //   height: '70%',
+      //   handleSize: 8,
+      //   // showDataShadow: false,
+      //   left: '93%',
+      // },
+    ],
     toolbox: {
       feature: {
         saveAsImage: {},
@@ -44,7 +75,24 @@ onMounted(() => {
         name: '销量',
         type: 'bar',
         data: props.dataY,
+        itemStyle: {
+          normal: {
+            barBorderRadius: [30, 30, 0, 0],
+            },
+          },
       },
+      // 柱状图上的线
+      {
+        // name: 'Temperature',
+        type: 'line',
+        // yAxisIndex: 10,
+        tooltip: {
+          valueFormatter: function (value) {
+            return value + ' %';
+          }
+        },
+        data: props.dataLine
+      }
     ],
   });
 });
--- src/views/component/NavigatorTwo.vue
+++ src/views/component/NavigatorTwo.vue
@@ -5,12 +5,12 @@
       <el-breadcrumb-item>数据分析</el-breadcrumb-item>
     </el-breadcrumb>
     <div class="echarts">
-      <echart-line class="echarts-left" :dataX="dataX" :dataY="dataY" :name="name"></echart-line>
+      <echart-line class="echarts-left" :dataX="dataX" :dataY="dataY" :name="name" ></echart-line>
       <echart-line class="echarts-right" :dataX="dataD" :dataY="dataDs" :name="name2"></echart-line>
     </div>
     <div class="echarts">
-      <echart-bar class="echarts-right" :dataX="dataD" :dataY="dataDs" :name="name2"> </echart-bar>
-      <echart-bar class="echarts-right" :dataX="dataX" :dataY="dataY" :name="name"> </echart-bar>
+      <echart-bar class="echarts-right" :dataX="dataD" :dataY="dataDs" :dataLine="dataLine1" :name="name2"> </echart-bar>
+      <echart-bar class="echarts-right" :dataX="dataX" :dataY="dataY" :dataLine="dataLine2" :name="name"> </echart-bar>
     </div>
   </div>
 </template>
@@ -21,10 +21,12 @@ import EchartLine from '../../config/EchartLine.vue';
 import EchartBar from '../../config/EchartBar.vue';
 const name = ref('2021年');
 const name2 = ref('2022年');
-const dataX = ref(['一月', '二月', '三月', '四月', '五月', '六月', '七月']);
-const dataY = ref([30, 20, 30, 50, 20, 40, 20]);
+const dataX = ref(['一月', '二月', '三月', '四月', '五月', '六月', '七月','8月', '9月', '10月', '11月', '12月', '13月', '14月']);
+const dataY = ref([30, 20, 30, 50, 20, 40, 20, 30, 20, 30, 50, 20, 40, 20]);
 const dataD = ref(['一月', '二月', '三月', '四月', '五月', '六月', '七月  ']);
 const dataDs = ref([10, 20, 30, 40, 20, 40, 20]);
+const dataLine1 = ref([10, 20, 30, 40, 20, 40, 20])
+const dataLine2 = ref([30, 20, 30, 50, 20, 40, 20, 30, 20, 30, 50, 20, 40, 20])
 </script>
 
 <style lang="scss" scoped>
